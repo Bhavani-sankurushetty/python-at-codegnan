678363039b547e3e47d2f33ef8af76e24bedf3ca
Bhavani-sankurushetty <lakshmibhavani123@gmail.com>
2026-07-20 16:46:53 +0530

    Python

---
+++ strings.py
@@ -0,0 +1,20 @@
+print("Strings intro")
+#Accessining the string character by using indexing
+s="Codegnan"
+print(s[0])
+print(s[3])
+print(s[-1])
+
+
+# string slicing
+print(s[:])
+print(s[:5])
+print(s[-1:-5:-1])
+print(s[-2:-6])
+print(s[4:-1])
+print(s[::-1])
+print(s[:-5:-1])
+print(s[-3::-1])
+print(s[-2::-1])
+print(s[1:6:2])
+print(s[-2:-6])
